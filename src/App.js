52d88f9f5db8a68096bb42762e8c2f23774f6de8
DongIn Kang <37896341+did0824@users.noreply.github.com>
2023-01-11 11:38:42 +0900

    레이아웃 수정

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
+import { useState, useMemo } from "react";
 import { MainButton, useShowPopup, useThemeParams } from "@vkruglikov/react-telegram-web-app";
 
 const App = () => {
     const showPopup = useShowPopup();
     const theme = useThemeParams();
     const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe || {};
+    const [optionList, setOptionList] = useState([]);
+
+    const canSubmit = useMemo(() => {
+        return initDataUnsafe?.user && optionList.length;
+    }, [initDataUnsafe, optionList]);
+
+    const giftList = [
+        { name: "상품1", price: 1000 },
+        { name: "상품2", price: 500 },
+        { name: "상품3", price: 200 },
+        { name: "상품4", price: 100 },
+        { name: "상품5", price: 50 },
+        { name: "상품6", price: 10 },
+    ];
 
     return (
-        <div className="App">
+        <div className="App" style={{ backgroundColor: theme[1].bg_color, color: theme[1].color }}>
             <div>
-                <p>initDataUnsafe</p>
-                <p>{JSON.stringify(initDataUnsafe)}</p>
+                <p>
+                    {initDataUnsafe?.user?.username ?? initDataUnsafe?.user?.last_name}님의 보유 포인트는 1000pt입니다!
+                </p>
             </div>
-            <div>
-                <p>theme</p>
-                <p>{JSON.stringify(theme)}</p>
+
+            <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr" }}>
+                {giftList.map((item, idx) => (
+                    <div key={idx}>
+                        <p>{item.name}</p>
+                        <img
+                            alt="상품"
+                            src="https://www.pngmart.com/files/17/Birthday-Gift-Box-PNG-File.png"
+                            width={200}
+                        />
+                    </div>
+                ))}
             </div>
+
             <MainButton
-                text="SHOW POPUP"
+                text="교환하기"
+                disable={!canSubmit}
                 onClick={() => {
                     showPopup({
                         message: "Hello, I'am showPopup handle",
